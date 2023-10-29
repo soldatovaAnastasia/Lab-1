c8f1a2de9e4c63dc1b9429369ad011e72a83564b
Soldatova Anastasia <anastasiasoldatova927@gmail.com>
2023-10-30 01:01:16 +0600

    1st item

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# hello
+
 RED = '\u001b[41m'
 BLUE = '\u001b[44m'
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
 
-# for i in range(6):
-#     if i < 3:
-#         print(f'{BLUE}{"  " * (2 * i + 2)}{WHITE}{"  " * (14 - 2 * i)}{END}')
-#     else:
-#         print(f'{BLUE}{"  " * (12 - 2 * i)}{RED}{"  " * (4 + 2 * i)}{END}')
+for i in range(6):
+     if i < 3:
+         print(f'{BLUE}{"  " * (2 * i + 2)}{WHITE}{"  " * (14 - 2 * i)}{END}')
+     else:
+         print(f'{BLUE}{"  " * (12 - 2 * i)}{RED}{"  " * (4 + 2 * i)}{END}')
 
 
 plot_list = [[0 for i in range(10)] for i in range(10)]
---
+++ solutionlab.py
@@ -0,0 +1,15 @@
+
+YELLOW = '\u001b[43m'
+GREEN = '\u001b[42m'
+RED = '\u001b[41m'
+END = '\u001b[0m'
+
+for i in range(10):
+    if i < 3:
+         print(f'{YELLOW}{"  " * (15)}{END}')
+    if 2 < i < 6:
+          print(f'{GREEN}{"  " * (15)}{END}')  
+    if i > 6:
+        print(f'{RED}{"  " * (15)}{END}') 
+            
+     
